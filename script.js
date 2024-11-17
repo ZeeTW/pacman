eb0ee3bae390433fbab505aa005d281debce8e31
ZeeTW <zainab.alafoo@hotmail.com>
2024-11-17 13:19:10 +0300

    fixed movement and did points thing

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
 const board = [
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-  [1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-  [1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
-  [1, 1, 1, 1, 0, 1, 0, 0, 1, 1],
-  [0, 0, 0, 0, 2, 1, 0, 0, 1, 1],
-  [3, 3, 3, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 0, 1, 1, 0, 1, 1],
-  [1, 0, 1, 0, 0, 1, 1, 0, 1, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-  [1, 0, 1, 1, 1, 1, 1, 1, 1, 1]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0, 1, 1, 0],
+  [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+  [1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+  [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+  [1, 3, 3, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 0, 0, 1, 1, 0, 1],
+  [1, 0, 1, 1, 0, 0, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 /*---------------------------- Variables (state) ----------------------------*/
 let pacmanPosition = {
   x: 4,
-  y: 5
+  y: 6
 }
+let direction = null
+let moveInt = null
+let score = 0
 
 /*------------------------ Cached Element References ------------------------*/
 const gameBoard = document.querySelector('.game')
@@ -45,39 +48,90 @@ const createBoard = () => {
   })
 }
 
+// const updatePosition = () => {
+//   document.addEventListener('keydown', (event) => {
+//     let newPositionX = pacmanPosition.x
+//     let newPositionY = pacmanPosition.y
+//     if (event.key === 'ArrowUp') {
+//       newPositionY -= 1
+//     } else if (event.key === 'ArrowDown') {
+//       newPositionY += 1
+//     } else if (event.key === 'ArrowLeft') {
+//       newPositionX -= 1
+//     } else if (event.key === 'ArrowRight') {
+//       newPositionX += 1
+//     } else {
+//       console.log('no')
+//     }
+
+//     if (
+//       newPositionX >= 0 &&
+//       newPositionY >= 0 &&
+//       newPositionY < board.length &&
+//       newPositionX < board[0].length &&
+//       board[newPositionY][newPositionX] !== 1
+//     ) {
+//       pacmanPosition = { x: newPositionX, y: newPositionY }
+//       createBoard()
+//     } else {
+//       console.log('hit a wall/out of boundaries')
+//     }
+//   })
+// }
+
 const updatePosition = () => {
-  document.addEventListener('keydown', (event) => {
-    let newPositionX = pacmanPosition.x
-    let newPositionY = pacmanPosition.y
-    if (event.key === 'ArrowUp') {
-      newPositionY -= 1
-    } else if (event.key === 'ArrowDown') {
-      newPositionY += 1
-    } else if (event.key === 'ArrowLeft') {
-      newPositionX -= 1
-    } else if (event.key === 'ArrowRight') {
-      newPositionX += 1
-    } else {
-      console.log('no')
-    }
+  let newPositionX = pacmanPosition.x
+  let newPositionY = pacmanPosition.y
+  if (direction === 'up') {
+    newPositionY -= 1
+  } else if (direction === 'down') {
+    newPositionY += 1
+  } else if (direction === 'left') {
+    newPositionX -= 1
+  } else if (direction === 'right') {
+    newPositionX += 1
+  } else {
+    console.log('no')
+  }
 
-    if (
-      newPositionX >= 0 &&
-      newPositionY >= 0 &&
-      newPositionY < board.length &&
-      newPositionX < board[0].length &&
-      board[newPositionY][newPositionX] !== 1
-    ) {
-      pacmanPosition = { x: newPositionX, y: newPositionY }
-    } else {
-      console.log('hit a wall/out of boundaries')
+  if (
+    newPositionX >= 0 &&
+    newPositionY >= 0 &&
+    newPositionY < board.length &&
+    newPositionX < board[0].length &&
+    board[newPositionY][newPositionX] !== 1
+  ) {
+    if (board[newPositionY][newPositionX] === 3) {
+      board[newPositionY][newPositionX] = 0
+      score += 100
+      console.log(`the score is: ${score}`)
     }
-
+    pacmanPosition = { x: newPositionX, y: newPositionY }
     createBoard()
-  })
+  } else {
+    console.log('hit a wall/out of boundaries')
+  }
 }
 
-createBoard()
-updatePosition()
+const startMove = (way) => {
+  if (direction !== way) {
+    direction = way
+    clearInterval(moveInt)
+    moveInt = setInterval(updatePosition, 400)
+  }
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowUp') {
+    startMove('up')
+  } else if (event.key === 'ArrowDown') {
+    startMove('down')
+  } else if (event.key === 'ArrowRight') {
+    startMove('right')
+  } else if (event.key === 'ArrowLeft') {
+    startMove('left')
+  }
+})
+
+createBoard()
